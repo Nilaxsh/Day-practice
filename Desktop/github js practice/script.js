5436dfa80221e6b7db86db002968c05e89a3aeb4
Nilaxsh <nilaxsakana@gmail.com>
2023-11-06 22:41:56 +0530

    secondcommit

---
+++ Desktop/github js practice/script.js
@@ -0,0 +1,80 @@
+//q1
+//comments can make code readable
+
+//q2
+//Welcome to learn Javascript
+
+//q3
+/*comments can make code readable, easy to reuse and informative*/
+
+//q4
+//string
+var string="nilaxsh";
+console.log(string);
+//number
+var number=18
+console.log(number);
+//boolean
+var boolean="true"
+console.log(boolean);
+//undefined
+var myname;
+console.log(myname);
+//
+var mynull=null
+console.log(mynull);
+
+
+//q5
+console.log(typeof"nilax");
+console.log(typeof 60);
+console.log(typeof true );
+var age;
+console.log(typeof age);
+console.log();
+
+
+//q6
+// var firstname;
+// var lastname;
+// var school_name
+// var age
+
+//q7
+// var first_name =  "Nilaxsh";
+// var last_name = "Kanesh";
+// var age = 24 ;
+// var study = "deoloper";
+
+//q8
+var first_name =  "Nilaxsh";
+var last_name = "Kanesh";
+var age = 24 ;
+var marital_status = "Single";
+var country = "srilanka";
+console.log(first_name,last_name,age,marital_status,country)
+
+//q9
+var firstname = "nilaxsh", lastname="kanesh",age=24,marital_status="single",country="srilanka";
+console.log(firstname,lastname,age,marital_status,country);
+
+
+//q10
+var myage=24;
+var yourage=26;
+console.log("my age is" ,myage);
+console.log("your age is" , yourage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
